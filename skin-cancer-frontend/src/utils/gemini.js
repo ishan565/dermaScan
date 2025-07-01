62a3617123cf301d2c89b91e55f4ee9859ac47f8
ishan565 <143117312+ishan565@users.noreply.github.com>
2025-07-01 12:53:57 +0530

    Update gemini.js

--- skin-cancer-frontend/src/utils/gemini.js
+++ skin-cancer-frontend/src/utils/gemini.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyD049wt1CQ4Q4eGxdJs9Ey6-u16rFa7G3k");
+const genAI = new GoogleGenerativeAI("your open ai key");
 
 export async function getAdvice(prompt) {
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
